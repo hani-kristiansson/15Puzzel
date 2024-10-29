de329123d133329751aad16f86766747bfc70a90
Hani <hani.kristiansson@gmail.com>
2024-10-29 20:41:41 +0100

    refactoring of buttons, initiating for loops

--- src/Puzzel15/GameBoard.java
+++ src/Puzzel15/GameBoard.java
@@ -5,50 +5,48 @@ import java.awt.*;
 
 public class GameBoard extends JFrame {
 
-    JPanel panel = new JPanel();
+    JPanel gamePanel = new JPanel();
+    JPanel menuPanel = new JPanel();
 
-    JButton button1 = new JButton("1");
-    JButton button2 = new JButton("2");
-    JButton button3 = new JButton("3");
-    JButton button4 = new JButton("4");
-    JButton button5 = new JButton("5");
-    JButton button6 = new JButton("6");
-    JButton button7 = new JButton("7");
-    JButton button8 = new JButton("8");
-    JButton button9 = new JButton("9");
-    JButton button10 = new JButton("10");
-    JButton button11 = new JButton("11");
-    JButton button12 = new JButton("12");
-    JButton button13 = new JButton("13");
-    JButton button14 = new JButton("14");
-    JButton button15 = new JButton("15");
-    JButton button16 = new JButton();
-
+    JButton[][] buttons = new JButton[4][4];
 
     public GameBoard() {
-        setLayout(new GridLayout(4,4));
-        add(button1);
-        add(button2);
-        add(button3);
-        add(button4);
-        add(button5);
-        add(button6);
-        add(button7);
-        add(button8);
-        add(button9);
-        add(button10);
-        add(button11);
-        add(button12);
-        add(button13);
-        add(button14);
-        add(button15);
-        add(button16);
 
+        setLayout(new BorderLayout());
+        add(menuPanel, BorderLayout.NORTH);
+        add(gamePanel, BorderLayout.CENTER);
+
+        gamePanel.setLayout(new GridLayout(4, 4));
+        menuPanel.setLayout(new GridLayout(1, 2));
+
+        //Game Panel Buttons
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                buttons[i][j] = new JButton();
+                buttons[i][j].setBackground(Color.lightGray);
+                buttons[i][j].setForeground(Color.white);
+                buttons[i][j].setFont(new Font("Arial", Font.BOLD, 20));
+                gamePanel.add(buttons[i][j]);
+            }
+        }
+
+        JButton startButton = new JButton("Start");
+        startButton.setBackground(Color.black);
+        startButton.setForeground(Color.white);
+        startButton.setFont(new Font("Arial", Font.BOLD, 25));
+        menuPanel.add(startButton);
+
+        JButton hintButton = new JButton("Hint pls");
+        hintButton.setBackground(Color.red);
+        hintButton.setForeground(Color.white);
+        hintButton.setFont(new Font("Arial", Font.BOLD, 25));
+        menuPanel.add(hintButton);
 
         setSize(400,400);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-
     }
+
+
 }
